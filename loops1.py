25684695a4013c7111333d98457b6ecb412b84bd
UdayChinamora <uchinamhora@gmail.com>
2023-10-15 15:09:30 -0500

    added if else loops and comments

--- loops1.py
+++ loops1.py
@@ -1,8 +1,17 @@
+
 #!/usr/bin/python
-# what is this doing? 
+# what is this doing?
+# This code uses nested loops to print the values of x and y
 for x in range(0, 8):
     for y in range(0, 16):
-      print x, y
+        if x < 4 and y < 8:
+            print(f"({x}, {y}) is in the first quadrant.")  # Prints a message if the coordinates are in the first quadrant
+        elif x >= 4 and y < 8:
+            print(f"({x}, {y}) is in the second quadrant.")  # Prints a message if the coordinates are in the second quadrant
+        elif x >= 4 and y >= 8:
+            print(f"({x}, {y}) is in the third quadrant.")  # Prints a message if the coordinates are in the third quadrant
+        else:
+            print(f"({x}, {y}) is in the fourth quadrant.")  # Prints a message if the coordinates are in the fourth quadrant
       
-        
+
   
--- loops2.py
+++ loops2.py
@@ -2,5 +2,10 @@
 # how is this?
 import random
 
-number =int(random.randint(1,6)) 
-print number
+number = random.randint(1, 6)
+
+# Check if the number is even or odd
+if number % 2 == 0:
+    print(f"{number}   Even number!")  # Prints a message if the number is even
+else:
+    print(f"{number}   Odd number!")  # Prints a message if the number is odd
