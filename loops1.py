992ae6990813447d671fabfd2d07175cb35e27c9
Riley D <111332720+ridodge@users.noreply.github.com>
2023-10-16 20:49:32 -0500

    Update loops1.py
    Answered the commented question. Also fixed the parenthesis for the print statement at line 5.

--- loops1.py
+++ loops1.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
-# what is this doing? 
+# what is this doing? This is a nested loop, for each index for 'x': 'y' processes 16 times. Each time 'y' processes, it prints the index it is on for both 'x' and 'y'.
 for x in range(0, 8):
     for y in range(0, 16):
-      print x, y
+      print(x, y)
       
         
   
